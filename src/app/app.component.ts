f8c6cf4c035e8cb03a232175eea3e52ce850ab25
Dimitri Balazs <dimi@Dimitris-MacBook-Pro.local>
2017-08-08 17:40:27 +0200

    updated project.js cli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { DatabaseService } from './database.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [DatabaseService]
 })
+
 export class AppComponent {
   title = 'app';
+
+  constructor(private databaseService: DatabaseService) { 
+    //this.databaseService.listen();
+
+  }
+
+  test(woot): void {
+    console.log(woot);
+    this.databaseService.saveTest();
+  }
 }

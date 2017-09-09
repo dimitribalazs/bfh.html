06ad99c5a4936195f7eb6f9896c480751c52b823
Beat Luginbühl <lugi@lugipfupf.ch>
2017-09-09 15:08:26 +0200

    DUF-42: add menuService state awareness

--- src/app/shared/components/menu-bar/menu-bar.component.ts
+++ src/app/shared/components/menu-bar/menu-bar.component.ts
@@ -1,15 +1,22 @@
 import {Component, NgModule, OnInit} from '@angular/core';
+import {MenuService} from '../../services/menu.service';
 
 @Component({
   selector: 'app-menu-bar',
   templateUrl: './menu-bar.component.html',
   styleUrls: ['./menu-bar.component.css'],
+  providers: [MenuService]
 })
 
 
 export class MenuBarComponent {
+  menuService: MenuService;
   toggleMenu = false;
 
+  constructor(menuService: MenuService) {
+    this.menuService = menuService;
+    menuService.setDefault();
+  }
 
   onToggleMenu() {
     this.toggleMenu = !this.toggleMenu;

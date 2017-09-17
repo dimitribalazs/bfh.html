9f2450a0497eda5f7c783b7c823c063f4e718963
stocker1893@gmail.com <Leha162g$>
2017-09-17 14:59:40 +0200

    eigener component für Available daten

--- src/app/bar/available-beers/available-beers.component.ts
+++ src/app/bar/available-beers/available-beers.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {BarService} from '../barService';
 import {ActivatedRoute, ParamMap, Router} from '@angular/router';
+import {BehaviorSubject} from 'rxjs/Rx';
+import {MultiNavigationModel} from '../../shared/domainModel/multiNavigationModel';
+import {BeerBarModel} from "../../shared/domainModel/viewModels";
 
 @Component({
   selector: 'app-available-beers',
@@ -10,15 +13,15 @@ import {ActivatedRoute, ParamMap, Router} from '@angular/router';
 export class AvailableBeersComponent implements OnInit {
   // beers: Observable<Beer[]>;
 
+  filter: number
   service: BarService
 
-  constructor(private barService: BarService, private router: Router,) {
+  constructor(private barService: BarService, private router: Router) {
     this.service = barService;
+    this.filter = 3
   }
 
   ngOnInit() {
-
-    // this.beers = this.barService.getAvailableBeers('1');
   }
 
   onAddBeer() {
@@ -28,6 +31,13 @@ export class AvailableBeersComponent implements OnInit {
 
   onShowBeer(id: string) {
     this.router.navigate(['beer', id]);
+  }
+
+  addBeer(id: string) {
+    console.log('******************************************add')
+  }
+  removeBeer(id: string) {
+    console.log('******************************************remove')
   }
 }
 

bf579b77bbdb1671a886ce101dc367369e55c4ad
Beat Luginbühl <lugi@lugipfupf.ch>
2017-09-20 22:10:08 +0200

    DUF-43: delete background image files

--- src/app/beer/beer-top10/beer-top10.component.ts
+++ src/app/beer/beer-top10/beer-top10.component.ts
@@ -1,21 +1,25 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Beer} from '../../shared/dto/beer';
 import {Observable} from 'rxjs/Observable';
 import {logger} from 'codelyzer/util/logger';
-import { Router } from '@angular/router';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-beer-top10',
   templateUrl: './beer-top10.component.html',
   styleUrls: ['./beer-top10.component.css']
 })
-export class BeerTop10Component {
+export class BeerTop10Component implements OnInit {
 
   @Input() title: String;
   @Input() beers: Observable<Beer>;
 
 
   constructor(private router: Router) {
+  }
+
+  ngOnInit(): void {
+    console.log(this.beers);
   }
 
   onSelect(beer: Beer) {

ac487dc5edef58435b8347d82315de943d858bce
Adrian Bachmann <boomerhimself@gmail.com>
2017-09-23 16:16:46 +0200

    Fix BarId für map ActivatedRoute

--- src/app/bar/bar-info/bar-info.component.ts
+++ src/app/bar/bar-info/bar-info.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Bar} from '../../shared/dto/bar';
 import {BarService} from '../barService';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-bar-info',
@@ -11,18 +11,21 @@ import {Router} from '@angular/router';
 export class BarInfoComponent implements OnInit {
   // model: Bar = new Bar;
   private showMap = false;
+  id: string;
 
-  constructor(private barService: BarService, private router: Router) {
+  constructor(private barService: BarService,
+              private router: Router,
+              private route: ActivatedRoute) {
   }
 
   ngOnInit() {
-    // this.barService.getBar().subscribe((beer) => {
-    //   this.model = this.barService.getViewModel();
-    // })
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
+    });
   }
 
   onClick(childView: string, activateNavigation: number) {
-    this.router.navigate(['bar', 1, childView]);
+    this.router.navigate(['bar', this.id, childView]);
   }
 
 }

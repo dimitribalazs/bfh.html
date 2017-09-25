093be91e6b243cb157824d08c92ca912c8b2d7eb
Adrian Bachmann <boomerhimself@gmail.com>
2017-09-25 22:36:03 +0200

    specs

--- src/app/beer/available-bars/available-bars.component.spec.ts
+++ src/app/beer/available-bars/available-bars.component.spec.ts
@@ -1,14 +1,45 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+// Import all AngularModules needed
+import { FormsModule } from '@angular/forms';
+
+
+
+// Provide ALL Services, and their dependencies
+import {BeerService} from '../beerService'
+import { BusinessService } from '../../shared/services/business.service';
+import {BeerDatabaseService} from '../../shared/services/beer.service';
+import {BreweryDatabaseService} from '../../shared/services/brewery.service';
+import {BarDatabaseService} from '../../shared/services/bar.service';
+import {UserDatabaseService} from '../../shared/services/user.service';
+import {GeoService} from '../../shared/services/geo.service';
+import {MenuService} from '../../shared/services/menu.service';
+import {BreweryService} from '../../brewery/breweryService';
+
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { AvailableDataComponent } from '../../shared/components/available/available-data/available-data.component';
+import { SearchResultComponent } from '../../shared/components/search-result/searchResult.component';
+import { LinkInformationComponent } from '../../shared/components/available/link-information/link-information.component';
+
+
 import { AvailableBeersComponent } from './available-bars.component';
 
-describe('AvailableBeersComponent', () => {
+describe('AvailableBars in BeerComponent', () => {
   let component: AvailableBeersComponent;
   let fixture: ComponentFixture<AvailableBeersComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AvailableBeersComponent ]
+      imports: [ FormsModule, RouterTestingModule ],
+      declarations: [ AvailableBeersComponent, AvailableDataComponent, SearchResultComponent, LinkInformationComponent ],
+      providers: [ BeerService, MenuService, BreweryService,
+        BusinessService,
+        BeerDatabaseService,
+        BreweryDatabaseService,
+        BarDatabaseService,
+        UserDatabaseService,
+        GeoService]
     })
     .compileComponents();
   }));
--- src/app/beer/beer-map/beer-map.component.spec.ts
+++ src/app/beer/beer-map/beer-map.component.spec.ts
@@ -1,5 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+// Import all AngularModules needed
+import { FormsModule } from '@angular/forms';
+
+import { RouterTestingModule } from '@angular/router/testing';
+
+// Provide ALL Services, and their dependencies
+import {BarService} from '../../bar/barService'
+import {BeerService} from '../beerService'
+import { BusinessService } from '../../shared/services/business.service';
+import {BeerDatabaseService} from '../../shared/services/beer.service';
+import {BreweryDatabaseService} from '../../shared/services/brewery.service';
+import {BarDatabaseService} from '../../shared/services/bar.service';
+import {UserDatabaseService} from '../../shared/services/user.service';
+import {GeoService} from '../../shared/services/geo.service';
+import {MenuService} from '../../shared/services/menu.service';
+
 import { BeerMapComponent } from './beer-map.component';
 
 describe('BeerMapComponent', () => {
@@ -8,7 +24,15 @@ describe('BeerMapComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BeerMapComponent ]
+      imports: [ FormsModule, RouterTestingModule ],
+      declarations: [ BeerMapComponent ],
+      providers: [ BeerService, MenuService, BarService,
+        BusinessService,
+        BeerDatabaseService,
+        BreweryDatabaseService,
+        BarDatabaseService,
+        UserDatabaseService,
+        GeoService]
     })
     .compileComponents();
   }));
